54169df9f29f9b64ec2528c9bb110e1d3c73be48
takahashi <shu.takahashi.sys22@morijyobi.ac.jp>
2023-07-03 12:07:01 +0900

    login

--- app.py
+++ app.py
@@ -1,14 +1,60 @@
-from flask import Flask, render_template, url_for, request
-import db
+from flask import Flask, render_template, url_for, request, redirect, session
+import db, string, random
 from book import book_bp
 
 app = Flask(__name__)
 
 app.register_blueprint(book_bp)
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def index():
-    return render_template('index.html')
+    msg = request.args.get('msg')
+    
+    if msg == None:
+        return render_template('index.html')
+    else:
+        return render_template('index.html', msg=msg)
+
+@app.route('/', methods = ['POST'])
+def login():
+    mail = request.form.get('mail')
+    password = request.form.get('password')
+    
+    if db.login(mail, password):
+        return redirect(url_for('product_list'))
+    else:
+        error = 'ログインに失敗しました。'
+        input_data = {
+            'mail' : mail,
+            'password' : password
+        }
+        return render_template('index.html', error=error, data=input_data)
+    
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+@app.route('/register_exe', methods=['POST'])
+def register_exe():
+    user_name = request.form.get('username')
+    mail = request.form.get('mail')
+    password = request.form.get('password')
+    
+    if user_name == '':
+        error = 'ユーザ名が未入力です。'
+        return render_template('register.html', error=error)
+    if password == '':
+        error = 'パスワードが未入力です。'
+        return render_template('register.html', error=error)
+    
+    count = db.insert_user(user_name, mail, password)
+    
+    if count == 1:
+        msg = '登録が完了しました。'
+        return render_template('index.html', msg=msg)
+    else:
+        error = '登録に失敗しました。'
+        return render_template('register.html', error=error)
 
 @app.route('/product_list')
 def product_list():
--- db.py
+++ db.py
@@ -1,10 +1,66 @@
-import os
-import psycopg2
+import os, psycopg2, string, hashlib, random
 
 def get_connection():
     url = os.environ['DATABASE_URL']
     connection = psycopg2.connect(url)
     return connection
+
+def get_salt():
+    charset = string.ascii_letters + string.digits
+    
+    salt = ''.join(random.choices(charset, k=30))
+    return salt
+
+def get_hash(password, salt):
+    b_pw = bytes(password, 'utf-8')
+    b_salt = bytes(salt, 'utf-8')
+    hashed_password = hashlib.pbkdf2_hmac('sha256', b_pw, b_salt, 1246).hex()
+    return hashed_password
+
+
+def insert_user(user_name, mail, password):
+    sql = 'INSERT INTO shopping_user VALUES (default, %s, %s, %s, %s)'
+    salt = get_salt()
+    hashed_password = get_hash(password, salt)
+    try: 
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (user_name, mail, hashed_password, salt))
+        count = cursor.rowcount
+        connection.commit()
+    except psycopg2.DatabaseError: 
+        count=0
+    finally:
+        cursor.close()
+        connection.close()
+        
+    return count
+
+def login(mail, password):
+    sql = 'SELECT hashed_password, salt FROM shopping_user WHERE mail ~ %s'
+    flg = False
+    
+    try :
+        connection = get_connection()
+        cursor = connection.cursor()
+        
+        cursor.execute(sql, (mail,))
+        user = cursor.fetchone()
+        
+        if user != None:
+            salt = user[1]
+            
+            hashed_password = get_hash(password, salt)
+            
+            if hashed_password == user[0]:
+                flg = True
+    except psycopg2.DatabaseError:
+        flg = False
+    finally:
+        cursor.close()
+        connection.close()
+    
+    return flg
 
 def select_all_merchandise():
     connection = get_connection()
